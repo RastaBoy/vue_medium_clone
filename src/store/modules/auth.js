e25dae0cfef112cb5123fa5b224374064056ee21
Alexander Troyan <trustyone3224@gmail.com>
2021-07-27 16:17:15 +0700

    veux get started

---
+++ src/store/modules/auth.js
@@ -0,0 +1,16 @@
+const state = {
+    isSubmitting: false
+}
+
+const mutations = {
+    registerStart(state) {
+        state.isSubmitting = true
+    }
+    
+}
+
+
+export default {
+    state,
+    mutations
+}
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -30,8 +30,10 @@
                 placeholder="Password" 
                />
             </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">Sign Up</button>
+            <button class="btn btn-lg btn-primary pull-xs-right" :disabled="isSubmitting">Sign Up</button>
+            {{ isSubmitting }}
           </form>
+          
         </div>
       </div>
     </div>
@@ -41,9 +43,15 @@
 <script>
 export default {
   name: 'McvRegister',
+  computed: {
+      isSubmitting() {
+          return this.$store.state.auth.isSubmitting
+      }
+  },
   methods: {
       onSubmit() {
           console.log("Submited form!")
+          this.$store.commit('registerStart')
       }
   }
 }
